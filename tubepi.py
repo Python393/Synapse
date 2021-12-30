3029f0cb17a196e133099011b97af36a07a11a0b
Python393 <badobobo06@gmail.com>
2021-12-30 23:23:52 +0100

    code

---
+++ tubepi.py
@@ -0,0 +1,15 @@
+# modules
+from typing import Annotated
+
+# annotations
+query = Annotated[str, "Search query for the google search engine."]
+
+# cheat sheet
+# https://serpapi.com/search.json?engine=google&q=Coffee&api_key=secret_api_key
+
+class synap(self):
+    def __init__(self):
+        pass
+
+    def loadUri(self, str(query)):
+        pass
